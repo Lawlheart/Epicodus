6c9d8c6ff1a4ec60892289326d42d6cf00e7962b
Kenneth Black <Kenneth.Black@live.com>
2016-11-23 14:27:18 -0800

    add final project, week 1 ruby

--- Ruby/week-1/wordcount/app.rb
+++ Ruby/week-1/wordcount/app.rb
@@ -3,7 +3,22 @@ require('sinatra/reloader')
 require('./lib/word_count')
 also_reload('lib/**/*.rb')
 
+set :public_folder, 'public'
 
 get('/') do
   erb(:index)
+end
+
+get('/analysis') do
+  @phrase = params.fetch('phrase')
+  @word = params.fetch('word')
+  @partials = params.has_key?('partials')
+
+  if @partials
+    @results = @phrase.word_count_partials @word
+  else
+    @results = @phrase.word_count @word
+  end
+
+  erb(:analysis)
 end
--- Ruby/week-1/wordcount/lib/word_count.rb
+++ Ruby/week-1/wordcount/lib/word_count.rb
@@ -1,5 +1,8 @@
 class String
-  define_method(:word_count) do
-
+  define_method(:word_count) do |match|
+    split(" ").keep_if{|w| w.downcase.gsub(/[^a-z ]/, '') == match.downcase}.length
+  end
+  define_method(:word_count_partials) do |match|
+    split(" ").keep_if{|w| w.downcase.include?(match.downcase)}.length
   end
 end
