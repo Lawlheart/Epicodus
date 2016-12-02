453704689cc36d454e379779b9a2235dd5867f88
LawlietBlack <Kenneth.Black@live.com>
2016-12-01 17:57:56 -0800

    Finish basic Address Class

--- Ruby/week-2/contacts/spec/address_spec.rb
+++ Ruby/week-2/contacts/spec/address_spec.rb
@@ -2,19 +2,22 @@ require('rspec')
 require('address')
 
 describe Address do
-  before(:each) do
-    address = Address.new({
+  before(:all) do
+    @address = Address.new({
       :local => "1234 Cat Street",
       :city => "Portland",
       :state => "Oregon",
-      :zip => 97221
+      :zip => 97221,
+      :type => "Home"
     })
   end
   describe("#initialize") do
-    expect(address.local).to eq "1234 Cat Street"
-    expect(address.city).to eq "Portland"
-    expect(address.state).to eq "Oregon"
-    expect(address.zip).to eq 97221
+    it("initializes the class with the information provided") do
+      expect(@address.local).to eq "1234 Cat Street"
+      expect(@address.city).to eq "Portland"
+      expect(@address.state).to eq "Oregon"
+      expect(@address.zip).to eq 97221
+    end
   end
 
 end
--- Ruby/week-2/contacts/spec/contact_spec.rb
+++ Ruby/week-2/contacts/spec/contact_spec.rb
@@ -2,18 +2,21 @@ require('rspec')
 require('contact')
 
 describe(Contact) do
+  before(:all) do
+    @brigette = Contact.new({
+      :first_name => "Brigette",
+      :last_name => "Eckert",
+      :job => "Front End Developer",
+      :company => "Phase2"
+    })
+  end
   describe("#initialize") do
     it('stores basic contact information') do
-      contact = Contact.new({
-        :first_name => "Brigette",
-        :last_name => "Eckert",
-        :job => "Front End Developer",
-        :company => "Phase2"
-      })
-      expect(contact.first_name).to eq "Brigette"
-      expect(contact.last_name).to eq "Eckert"
-      expect(contact.job).to eq "Front End Developer"
-      expect(contact.company).to eq "Phase2"
+
+      expect(@brigette.first_name).to eq "Brigette"
+      expect(@brigette.last_name).to eq "Eckert"
+      expect(@brigette.job).to eq "Front End Developer"
+      expect(@brigette.company).to eq "Phase2"
     end
   end
 end
