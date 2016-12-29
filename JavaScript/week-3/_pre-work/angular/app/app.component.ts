45eff340394d9c43c9722fc78959cdd7219b3511
Kenneth Black <kenneth.black@live.com>
2016-12-28 19:25:17 -0800

    finish angular skeleton

---
+++ JavaScript/week-3/_pre-work/angular/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>My First Angular 2 App</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
